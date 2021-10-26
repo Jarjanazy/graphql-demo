488cdd7190e48d9203202d1dc4504483a797cf7b
cercenazi <celil.cercenazi@gmail.com>
2021-10-26 22:38:17 +0300

    make the mutation add the entity to DB

--- src/main/java/jalil/demo/graphqldemo/api/graphql/mutation/BankAccountMutation.java
+++ src/main/java/jalil/demo/graphqldemo/api/graphql/mutation/BankAccountMutation.java
@@ -6,17 +6,23 @@ import jalil.demo.graphqldemo.api.graphql.dto.CreateBankAccountInput;
 import jalil.demo.graphqldemo.domain.entity.BankAccount;
 import jalil.demo.graphqldemo.domain.entity.Client;
 import jalil.demo.graphqldemo.domain.enums.Currency;
+import jalil.demo.graphqldemo.domain.repo.BankAccountRepo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@RequiredArgsConstructor
 public class BankAccountMutation implements GraphQLMutationResolver
 {
+    private final BankAccountRepo bankAccountRepo;
 
     BankAccount createBankAccount(CreateBankAccountInput input, DataFetchingEnvironment environment)
     {
         Client client = Client.builder().firstName(input.getFirstName()).lastName(input.getLastName()).build();
 
-        return BankAccount.builder().client(client).currency(Currency.DL).build();
+        BankAccount bankAccount = BankAccount.builder().client(client).currency(Currency.DL).build();
+
+        return bankAccountRepo.save(bankAccount);
     }
 }
